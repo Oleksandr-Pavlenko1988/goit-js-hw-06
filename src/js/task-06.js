112befa3a61c22fd285e8d3e439e026cad3030b5
Oleksandr Pavlenko <elvenmonuskrypt@gmail.com>
2023-03-09 15:50:44 +0200

    added function in task-6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,10 +3,13 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', e => {
     const currentValue = e.currentTarget.value.length;
     if (currentValue === Number(inputEl.dataset.length)) {
-        inputEl.classList.add('valid');
-        inputEl.classList.remove('invalid');
+        inputClassReplacement('valid', 'invalid');
     } else {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
+        inputClassReplacement('invalid', 'valid');
     }
 });
+
+function inputClassReplacement(add, remove) {
+    inputEl.classList.add(add);
+    inputEl.classList.remove(remove);
+};
